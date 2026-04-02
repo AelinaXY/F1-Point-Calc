d800fddc56503da6a70f9261e1be2f9337496030
Lauren Darlaston <lauren.darlaston@autotrader.co.uk>
2026-04-02 13:14:11 +0100

    Improve model performance

--- src/main/java/org/f1/regression/EvaluationResult.java
+++ src/main/java/org/f1/regression/EvaluationResult.java
@@ -8,6 +8,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.mllib.regression.LabeledPoint;
 import org.apache.spark.mllib.tree.GradientBoostedTrees;
 import org.apache.spark.mllib.tree.configuration.BoostingStrategy;
+import org.apache.spark.mllib.tree.model.DecisionTreeModel;
 import org.apache.spark.mllib.tree.model.GradientBoostedTreesModel;
 import org.f1.service.RegressionService;
 import scala.Tuple2;
@@ -36,55 +37,75 @@ public class EvaluationResult {
         List<HyperParameters> paramGrid = generateParameterGrid();
         dataSet.cache();
 
+        List<Tuple2<JavaRDD<LabeledPoint>, JavaRDD<LabeledPoint>>> folds = new ArrayList<>();
+        long seedBase = System.currentTimeMillis();
+        for (int i = 0; i < numFolds; i++) {
+            JavaRDD<LabeledPoint>[] splits = dataSet.randomSplit(
+                    new double[]{0.8, 0.2}, i * 1000L + seedBase
+            );
+            folds.add(new Tuple2<>(splits[0], splits[1]));
+        }
+
         AtomicInteger count = new AtomicInteger(0);
 
-        Set<EvaluationResult> results = paramGrid.parallelStream().map(params -> {
+        Set<EvaluationResult> results = paramGrid.parallelStream().flatMap(params -> {
             int currentCount = count.incrementAndGet();
 
-            double totalMSE = 0.0;
-            double totalMAE = 0.0;
-            double totalR2 = 0.0;
+            Map<Integer, double[]> metricsMap = new HashMap<>();
+            int[] iterationsToCheck = {25, 50, 100};
+            for (int iter : iterationsToCheck) {
+                metricsMap.put(iter, new double[3]);
+            }
 
-            for (int fold = 0; fold < numFolds; fold++) {
-                JavaRDD<LabeledPoint>[] splits = dataSet.randomSplit(
-                        new double[]{0.8, 0.2}, fold * 1000L + System.currentTimeMillis()
+            for (Tuple2<JavaRDD<LabeledPoint>, JavaRDD<LabeledPoint>> fold : folds) {
+                JavaRDD<LabeledPoint> training = fold._1();
+                JavaRDD<LabeledPoint> testing = fold._2();
 
-                );
-                JavaRDD<LabeledPoint> training = splits[0];
-                JavaRDD<LabeledPoint> testing = splits[1];
+                BoostingStrategy boostingStrategy = BoostingStrategy.defaultParams("Regression");
+                boostingStrategy.setNumIterations(params.getNumIterations());
+                boostingStrategy.setLearningRate(params.getLearningRate());
+                boostingStrategy.treeStrategy().setMaxDepth(params.getMaxDepth());
+                boostingStrategy.treeStrategy().setSubsamplingRate(params.getSubsamplingRate());
+                boostingStrategy.treeStrategy().setCategoricalFeaturesInfo(categoricalFeaturesInfo);
+                boostingStrategy.treeStrategy().setMinInstancesPerNode(params.getMinInstancesPerNode());
+                boostingStrategy.treeStrategy().setMinInfoGain(0.01);
+                boostingStrategy.treeStrategy().setMaxBins(32);
 
-                try {
-                    BoostingStrategy boostingStrategy = BoostingStrategy.defaultParams("Regression");
-                    boostingStrategy.setNumIterations(params.getNumIterations());
-                    boostingStrategy.setLearningRate(params.getLearningRate());
-                    boostingStrategy.treeStrategy().setMaxDepth(params.getMaxDepth());
-                    boostingStrategy.treeStrategy().setSubsamplingRate(params.getSubsamplingRate());
-                    boostingStrategy.treeStrategy().setCategoricalFeaturesInfo(categoricalFeaturesInfo);
-                    boostingStrategy.treeStrategy().setMinInstancesPerNode(params.getMinInstancesPerNode());
-                    boostingStrategy.treeStrategy().setMinInfoGain(0.01);
-                    boostingStrategy.treeStrategy().setMaxBins(32);
+                GradientBoostedTreesModel model = GradientBoostedTrees.train(training, boostingStrategy);
 
-                    GradientBoostedTreesModel model = GradientBoostedTrees.train(training, boostingStrategy);
+                for (int iter : iterationsToCheck) {
+                    GradientBoostedTreesModel slicedModel = (iter == params.getNumIterations()) ?
+                            model : sliceModel(model, iter);
 
                     JavaPairRDD<Double, Double> predictionAndLabel = testing.mapToPair(
-                            p -> new Tuple2<>(model.predict(p.features()), p.label())
+                            p -> new Tuple2<>(slicedModel.predict(p.features()), p.label())
                     );
 
-                    totalMSE += getMeanSquaredError(predictionAndLabel);
-                    totalMAE += getMeanAbsoluteError(predictionAndLabel);
-                    totalR2 += getRSquared(predictionAndLabel);
-                } finally {
-                    training.unpersist();
-                    testing.unpersist();
+                    double[] m = metricsMap.get(iter);
+                    m[0] += getMeanSquaredError(predictionAndLabel);
+                    m[1] += getMeanAbsoluteError(predictionAndLabel);
+                    m[2] += getRSquared(predictionAndLabel);
                 }
             }
-            logger.info(String.format("Version %d of %d. Params of %s. Total mean error: %f", currentCount, paramGrid.size(), params, totalMSE / numFolds));
-            return new EvaluationResult(
-                    params,
-                    totalMSE / numFolds,
-                    totalMAE / numFolds,
-                    totalR2 / numFolds
-            );
+
+            return Arrays.stream(iterationsToCheck).mapToObj(iter -> {
+                double[] m = metricsMap.get(iter);
+                HyperParameters currentParams = (iter == params.getNumIterations()) ? params :
+                        new HyperParameters(iter, params.getMaxDepth(), params.getLearningRate(),
+                                params.getMinInstancesPerNode(), params.getSubsamplingRate());
+
+                if (iter == 100) {
+                    logger.info(String.format("Version %d of %d. Params of %s. Total mean error: %f",
+                            currentCount, paramGrid.size(), currentParams, m[0] / numFolds));
+                }
+
+                return new EvaluationResult(
+                        currentParams,
+                        m[0] / numFolds,
+                        m[1] / numFolds,
+                        m[2] / numFolds
+                );
+            });
         }).collect(Collectors.toSet());
 
 
@@ -96,10 +117,16 @@ public class EvaluationResult {
         return bestResult.orElseThrow(() -> new RuntimeException("No results found"));
     }
 
+    private static GradientBoostedTreesModel sliceModel(GradientBoostedTreesModel model, int numTrees) {
+        DecisionTreeModel[] trees = Arrays.copyOf(model.trees(), numTrees);
+        double[] weights = Arrays.copyOf(model.treeWeights(), numTrees);
+        return new GradientBoostedTreesModel(model.algo(), trees, weights);
+    }
+
     private static List<HyperParameters> generateParameterGrid() {
         List<HyperParameters> paramGrid = new ArrayList<>();
 
-        int[] numIterations = {25, 50, 100};
+        int[] numIterations = {100};
         int[] maxDepths = {3, 5, 7};
         double[] learningRates = {0.01, 0.03, 0.1};
         int[] minInstancesPerNode = {1, 3, 5};
